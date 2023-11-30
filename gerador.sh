ab279f0c03b52a63e361e8c39e7a4b3a3543cc8d
dk <joseeduardopereznegrete1@gmail.com>
2023-11-30 05:06:33 -0600

    xd

--- gerador.sh
+++ gerador.sh
@@ -370,31 +370,29 @@ list_fix() {
     [[ ! -z $IPFIX ]] && echo "$IPFIX" >${DIR}/${KEY}/keyfixa
     echo -e "-------------------------------------------------"
 }
-
-ofus() {
-    unset txtofus
-    number=$(expr length $1)
-    for ((i = 1; i < $number + 1; i++)); do
-        txt[$i]=$(echo "$1" | cut -b $i)
-        case ${txt[$i]} in
-        ".") txt[$i]="x" ;;
-        "x") txt[$i]="." ;;
-        "5") txt[$i]="s" ;;
-        "s") txt[$i]="5" ;;
-        "1") txt[$i]="@" ;;
-        "@") txt[$i]="1" ;;
-        "2") txt[$i]="?" ;;
-        "?") txt[$i]="2" ;;
-        "4") txt[$i]="0" ;;
-        "0") txt[$i]="4" ;;
-        "/") txt[$i]="K" ;;
-        "K") txt[$i]="/" ;;
-        esac
-        txtofus+="${txt[$i]}"
-    done
-    echo "$txtofus" | rev
+ofus () {
+unset server
+server=$(echo ${txt_ofuscatw}|cut -d':' -f1)
+unset txtofus
+number=$(expr length $1)
+for((i=1; i<$number+1; i++)); do
+txt[$i]=$(echo "$1" | cut -b $i)
+case ${txt[$i]} in
+".")txt[$i]="C";;
+"C")txt[$i]=".";;
+"3")txt[$i]="@";;
+"@")txt[$i]="3";;
+"5")txt[$i]="9";;
+"9")txt[$i]="5";;
+"6")txt[$i]="D";;
+"D")txt[$i]="6";;
+"J")txt[$i]="Z";;
+"Z")txt[$i]="J";;
+esac
+txtofus+="${txt[$i]}"
+done
+echo "$txtofus" | rev
 }
-
 fix_key() {
     unset nomkey
     echo " Bienvenido, Porfavor dijita el Nombre del DUE�O de la KEYs"
